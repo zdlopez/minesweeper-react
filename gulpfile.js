c7ea6ebf8a21cad569c7e762c49e91bcf5f64189
zdlopez <zachary.d.lopez@gmail.com>
2015-04-03 14:09:18 -0700

    fix gulp issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,58 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy:false});
 var path = require('./paths.json');
-
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe($.react())
-    .pipe(gulp.dest(path.DEST_SRC));
-});
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('streamify');
 
 gulp.task('copy', function(){
   gulp.src(path.HTML)
     .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 
 gulp.task('default', ['watch']);
 
+
 gulp.task('build', function(){
-  gulp.src(path.JS)
-    .pipe($.react())
-    .pipe($.concat(path.MINIFIED_OUT))
-    .pipe($.uglify(path.MINIFIED_OUT))
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify($.uglify(path.MINIFIED_OUT)))
     .pipe(gulp.dest(path.DEST_BUILD));
 });
 
 gulp.task('replaceHTML', function(){
   gulp.src(path.HTML)
-    .pipe(htmlreplace({
+    .pipe($.htmlreplace({
       'js': 'build/' + path.MINIFIED_OUT
     }))
     .pipe(gulp.dest(path.DEST));
-}); 
+});
 
 gulp.task('production', ['replaceHTML', 'build']);
-
